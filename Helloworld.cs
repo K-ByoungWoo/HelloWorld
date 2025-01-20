a3343791be39dad624c791b91574cddf2fe10d92
SIRU <rkdquddn584@navere.com>
2025-01-20 16:05:15 +0900

    커밋좀 되라

--- Helloworld.cs
+++ Helloworld.cs
@@ -3,6 +3,7 @@ class Helloworld
     int main()
     {
         System.Console.WriteLine("Hello, World!");
+        System.Console.WriteLine("Press any key to continue...");
         return 0;
     }
 }
